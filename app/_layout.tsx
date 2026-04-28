9375fb737d9e6658572b4784d961e0bea0ce7b7d
Craig-Turley <craig.j.turley@gmail.com>
2026-04-28 16:56:37 +0900

    Setup expo-router

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,13 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
-import './global.css';
+import '@/global.css';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AppThemeProvider } from 'theme/theme-provider';
+import { Stack } from 'expo-router';
 
 export default function App() {
   return (
     <AppThemeProvider>
       <SafeAreaProvider>
-        <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-        <StatusBar style="auto" />
+        <Stack />
       </SafeAreaProvider>
     </AppThemeProvider>
   );
