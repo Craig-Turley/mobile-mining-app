f2b1e8c60b73aa602592e2f5083da38189be7d42
Craig-Turley <craig.j.turley@gmail.com>
2026-05-05 13:04:52 +0900

    Modifies scroll logic to use more efficient flat list component

--- features/video-player/components/subtitles-player.tsx
+++ features/video-player/components/subtitles-player.tsx
@@ -1,61 +1,22 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, FlatList, Button } from "react-native";
 import { useVideoPlayerContext } from "../contexts/video-screen-context";
 import { File } from 'expo-file-system';
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { parseSubtitles, SubtitleCue } from "@/utils/subtitles";
 import Subtitle from "./subtitle";
 
-// TODO: remove
-const fallbackSubtitle: SubtitleCue = {
-  id: -1,
-  start: 0,
-  end: 0,
-  text: "",
-};
-
 export default function SubtitlesPlayer() {
   const { subtitlesUri, timeStamp } = useVideoPlayerContext();
+
   const [subtitles, setSubtitles] = useState<SubtitleCue[]>([]);
-  const scrollViewRef = useRef<ScrollView | null>(null);
-  const itemPositions = useRef<Record<number, number>>({});
+  const subtitleListRef = useRef<FlatList<SubtitleCue> | null>(null);
 
-  // TODO: relook at this
-  const activeSubtitle = useMemo<SubtitleCue>(() => {
-    let previous: SubtitleCue | undefined;
+  const [activeSubtitleIndex, setActiveSubtitleIndex] = useState<number>(-1);
 
-    for (const subtitle of subtitles) {
-      if (timeStamp >= subtitle.start && timeStamp <= subtitle.end) {
-        return subtitle;
-      }
-
-      if (subtitle.end < timeStamp) {
-        previous = subtitle;
-      }
-
-      if (subtitle.start > timeStamp) {
-        break;
-      }
-    }
-
-    return previous ?? subtitles[0] ?? fallbackSubtitle;
-  }, [subtitles, timeStamp]);
-
-  const activeIndex = useMemo(() => {
-    return subtitles.findIndex((cue) => cue.id === activeSubtitle.id);
-  }, [subtitles, activeSubtitle]);
-
-  useEffect(() => {
-    if (activeIndex < 0) return;
-
-    const y = itemPositions.current[activeIndex];
-
-    if (y == null) return;
-
-    scrollViewRef.current?.scrollTo({
-      y: Math.max(y - 120, 0),
-      animated: true,
-    });
-  }, [activeIndex]);
+  async function readFile() {
+    const file = new File(subtitlesUri!);
+    return await file.text();
+  };
 
   useEffect(() => {
     const loadSubtitles = async () => {
@@ -67,6 +28,26 @@ export default function SubtitlesPlayer() {
     loadSubtitles();
   }, [subtitlesUri]);
 
+  useEffect(() => {
+    if (activeSubtitleIndex < 0) return;
+
+    subtitleListRef.current?.scrollToIndex({
+      index: activeSubtitleIndex,
+      animated: true,
+      viewPosition: 0,
+    });
+  }, [activeSubtitleIndex]);
+
+  useEffect(() => {
+    setActiveSubtitleIndex((prev) => {
+      const currentIndex = subtitles.findIndex(
+        (cue) => cue.start <= timeStamp && timeStamp <= cue.end
+      );
+
+      return currentIndex !== -1 ? currentIndex : prev;
+    });
+  }, [timeStamp, subtitles]);
+
   // TODO: style this
   if (subtitlesUri == null) {
     return (
@@ -76,24 +57,33 @@ export default function SubtitlesPlayer() {
     );
   }
 
-  async function readFile() {
-    const file = new File(subtitlesUri!);
-    return await file.text();
-  };
-
   return (
-    <View className="flex-1 items-center justify-center bg-background p-2">
-      <ScrollView className="w-full" ref={scrollViewRef}>
-        {subtitles.map((cue, index) => (
-          <Subtitle
-            key={cue.id}
-            cue={cue}
-            onLayout={(event) => {
-              itemPositions.current[index] = event.nativeEvent.layout.y;
-            }}
-          />
-        ))}
-      </ScrollView>
-    </View>
+    <>
+      <Button title="Set to last" onPress={() => setActiveSubtitleIndex(5)} />
+      <View className="flex-1 bg-background p-2">
+        <FlatList
+          ref={subtitleListRef}
+          data={subtitles}
+          keyExtractor={(item) => `${item.id}`}
+          renderItem={({ item }) => (
+            <Subtitle cue={item} />
+          )}
+          onScrollToIndexFailed={(info) => {
+            subtitleListRef.current?.scrollToOffset({
+              offset: info.averageItemLength * info.index,
+              animated: true,
+            });
+
+            setTimeout(() => {
+              subtitleListRef.current?.scrollToIndex({
+                index: info.index,
+                animated: true,
+                viewPosition: 0,
+              });
+            }, 250);
+          }}
+        />
+      </View>
+    </>
   );
 }
