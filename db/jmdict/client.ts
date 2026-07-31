902dde9553c01bd047ae356fe977da49b6224966
Craig-Turley <craig.j.turley@gmail.com>
2026-07-31 22:27:57 +0900

    move all jmdict generated db files to db/jmdict/generated

--- db/jmdict/client.ts
+++ db/jmdict/client.ts
@@ -1,6 +1,6 @@
 import { openDatabaseSync } from 'expo-sqlite';
 import { drizzle } from 'drizzle-orm/expo-sqlite';
-import * as schema from '@/drizzle/jmdict/schema';
+import * as schema from './generated/schema';
 
 export const JMDICT_DB_NAME = 'jmdict-v1.db';
 const sqlite = openDatabaseSync(JMDICT_DB_NAME);
--- drizzle.jmdict.config.ts
+++ drizzle.jmdict.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   schema: './db/jmdict/schema.ts',
-  out: './db/jmdict',
+  out: './db/jmdict/generated',
   dialect: 'sqlite',
   dbCredentials: {
     url: './dictionary_scripts/dictionaries/jmdict-v1.db',
